71ffe3a547217c6e8af6cb387d8c1645431a60c4
Kampfschaf <rpgtiger2k3@msn.com>
2015-07-12 20:14:53 +0200

    Fix path for Debug and Release Path

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DarkunderdoG, PlayKampfschaf")]
-[assembly: AssemblyProduct("JailPrison2")]
+[assembly: AssemblyProduct("JailPrison")]
 [assembly: AssemblyCopyright("Copyright © DarkunderdoG 2011-2014, PlayKampfschaf 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
